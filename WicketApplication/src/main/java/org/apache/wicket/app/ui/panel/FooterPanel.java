1e024be70f2e56ad52b61ebc65dc1589e21fdd00
edison.martins <martinsegmj@gmail.com>
2015-08-19 16:40:51 -0300

    Adicionado FooterPanel e MenuPanel

---
+++ WicketApplication/src/main/java/org/apache/wicket/app/ui/panel/FooterPanel.java
@@ -0,0 +1,17 @@
+package org.apache.wicket.app.ui.panel;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class FooterPanel extends Panel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FooterPanel(String id) {
+		super(id);
+		System.out.println("FooterPanel");
+	}
+
+}
