f91280aab4237a672be3f621e6d1ac4865635a40
edison.martins <martinsegmj@gmail.com>
2015-08-19 16:57:02 -0300

    Adicionado LoginPanel

---
+++ WicketApplication/src/main/java/org/apache/wicket/app/ui/panel/LoginPanel.java
@@ -0,0 +1,17 @@
+package org.apache.wicket.app.ui.panel;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class LoginPanel extends Panel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public LoginPanel(String id) {
+		super(id);
+		System.out.println("LoginPanel");
+	}
+
+}
