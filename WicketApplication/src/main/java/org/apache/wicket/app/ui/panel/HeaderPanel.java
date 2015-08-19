d6d0bc705c95ecdb005ddb0895275f7845e5a0d7
edison.martins <martinsegmj@gmail.com>
2015-08-19 16:30:47 -0300

    Adicionado HeaderPanel

---
+++ WicketApplication/src/main/java/org/apache/wicket/app/ui/panel/HeaderPanel.java
@@ -0,0 +1,17 @@
+package org.apache.wicket.app.ui.panel;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class HeaderPanel extends Panel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public HeaderPanel(String id) {
+		super(id);
+		System.out.println("HeaderPanel");
+	}
+
+}
