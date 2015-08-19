5f22e1991f8cab747a7b73de096e8a3aa78bc5c1
edison.martins <martinsegmj@gmail.com>
2015-08-19 17:15:52 -0300

    readicionado o LoginPage

---
+++ WicketApplication/src/main/java/org/apache/wicket/app/ui/LoginPage.java
@@ -0,0 +1,23 @@
+package org.apache.wicket.app.ui;
+
+import org.apache.wicket.app.ui.panel.LoginPanel;
+import org.apache.wicket.app.ui.template.JugTemplate;
+
+public class LoginPage extends JugTemplate {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	public LoginPage() {
+		
+		super();
+		System.out.println("LoginPage");
+		replace(new LoginPanel(CONTENT_ID));
+		getMenuPanel().setVisible(false);
+		
+	}
+
+}
