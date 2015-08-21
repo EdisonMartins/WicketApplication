872a303c013956518eeaea983dfb6c8fd7ab943a
edison.martins <martinsegmj@gmail.com>
2015-08-21 09:30:08 -0300

    Commit inicial

---
+++ BeginningWicket/src/main/java/org/apache/wicket/app/ui/panel/MenuPanel.java
@@ -0,0 +1,17 @@
+package org.apache.wicket.app.ui.panel;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class MenuPanel extends Panel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public MenuPanel(String id) {
+		super(id);
+		System.out.println("MenuPanel");
+	}
+
+}
